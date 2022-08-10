0d52b21404c6036aeb4cfd8deac6f8fd72c88b03
Janm20 <jan.mackovic@datablast.eu>
2022-08-10 18:01:41 +0200

    done

---
+++ comps/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyrights 2021 Ninja List</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas about" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          earum amet eos ipsa officia. Aliquid voluptate perferendis error illo
+          adipisci mollitia, eius culpa reiciendis deleniti nam atque voluptates
+          porro quis?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Consequuntur, tempore ab. Tempora ipsam nesciunt neque quisquam.
+          Illum, necessitatibus velit? Adipisci a recusandae minima, facilis
+          accusantium quisquam veniam earum maxime. Libero?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
